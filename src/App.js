800345e9a7332980e179bc445016450cbece0e43
Cedar Daniel <Cedardaniel1998@gmail.com>
2021-11-13 02:35:38 +0100

    styled login component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route, Routes } from 'react-router';
 import Homepage from './pages/homepage/homepage.component';
 import Shopage from './pages/shopage/shop.component';
 import Header from './components/header/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/shop" element={<Shopage />} />
+        <Route path="/signin" element={<SignInAndSignUpPage />} />
       </Routes>
     </div>
   );
