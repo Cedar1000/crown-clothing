18614d1a391e11c8d1fd3ea9f5e76c99daa57c55
Cedar Daniel <cedardaniel1998@gmail.com>
2022-04-27 10:54:14 +0100

    updated code to be pwa compliant, added service worker, https, uodated manifest

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './redux/store';
+import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import App from './App';
@@ -20,6 +21,8 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -11,6 +12,7 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV !== 'production') {
@@ -32,6 +34,10 @@ app.post('/payment', (req, res) => {
     amount: req.body.amount,
     currency: 'usd',
   };
+
+  app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+  });
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
     if (stripeErr) {
